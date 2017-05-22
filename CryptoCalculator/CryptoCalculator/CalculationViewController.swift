1b1132fa33f00f0f808a53bc71367af9581513ee
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-05-22 13:39:10 -0600

    Add ModelController/NetworlController/Model

---
+++ CryptoCalculator/CryptoCalculator/CalculationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CalculationViewController.swift
+//  CryptoCalculator
+//
+//  Created by Bradley GIlmore on 5/22/17.
+//  Copyright © 2017 Bradley Gilmore. All rights reserved.
+//
+
+import UIKit
+
+class CalculationViewController: UIViewController {
+
+    
+    
+}
